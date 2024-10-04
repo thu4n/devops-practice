4cd4ebb6bc7b042820c1ac96842bd9672dbb850b
thu4n <thuan.tongg@gmail.com>
2024-10-04 23:05:06 +0700

    update data models and add some sample data

--- backend/models/models.go
+++ backend/models/models.go
@@ -11,30 +11,34 @@ type User struct {
 	Name   string
 	Email  string
 	Orders []Order
-	// No password needed for this simple app
 }
 
 type Drink struct {
 	gorm.Model
 	Name        string
 	Description string
-	Size        string
-	Price       float32
+	DrinkSizes  []DrinkSize // Each drink can have multiple sizes
+}
+
+type DrinkSize struct {
+	gorm.Model
+	DrinkID uint    `gorm:"foreignKey:DrinkID;references:ID"`
+	Size    string  // "Small", "Medium", "Large"
+	Price   float32 // Price for this size
 }
 
 type Order struct {
 	gorm.Model
 	UserID      uint `gorm:"foreignKey:UserID;references:ID"`
 	OrderDate   time.Time
-	Size        string
 	OrderStatus string
 	OrderItems  []OrderItem
 }
 
 type OrderItem struct {
 	gorm.Model
-	OrderID  uint `gorm:"foreignKey:OrderID;references:ID"`
-	DrinkID  uint `gorm:"foreignKey:DrinkID;references:ID"`
-	Quantity uint
-	Price    float32
+	OrderID     uint `gorm:"foreignKey:OrderID;references:ID"`
+	DrinkSizeID uint `gorm:"foreignKey:DrinkSizeID;references:ID"`
+	Quantity    uint
+	Price       float32
 }
